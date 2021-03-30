c568d400ad773c5ff167761d74757f5521723559
LiamParmar <Liam.Parmar13@gmail.com>
2021-03-29 23:35:52 -0600

    Liam Test

--- src/edu/ucalgary/ensf409/Test.java
+++ src/edu/ucalgary/ensf409/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Testing code");
+        System.out.print("HI");
     }
 }
