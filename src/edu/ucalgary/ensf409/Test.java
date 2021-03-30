332ebb3fd8b508210b889147912d21885ed8ad29
Maheen <maheen.shossain@gmail.com>
2021-03-29 23:28:40 -0600

    Maheen's test

--- src/edu/ucalgary/ensf409/Test.java
+++ src/edu/ucalgary/ensf409/Test.java
@@ -3,5 +3,6 @@ package edu.ucalgary.ensf409;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Testing code");
     }
 }
