a3b6795a329c32815723376a3467d3cbda9be254
curtissilva <curtissilva77@gmail.com>
2021-03-30 23:10:54 -0600

    Methods displayLamps() and displayDesks()

--- src/edu/ucalgary/ensf409/Test.java
+++ src/edu/ucalgary/ensf409/Test.java
@@ -10,4 +10,22 @@ public class Test {
         System.out.println("Divyansh Branch Test");
         System.out.println("Curtis Branch Test");
     }
+
+    public void displayDesks()
+    {
+        System.out.println("You selected desks as your furniture.\n" +
+        "Please select from the following types of chairs:\n" + 
+        "1. Standing\n" +
+        "2. Traditional\n" +
+        "3. Adjustable");
+    }
+
+    public void displayLamps()
+    {
+        System.out.println("You selected Lamps as your furniture.\n" +
+        "Please select from the following types of chairs: \n" + 
+        "1. Desk\n" +
+        "2. Swing Arm\n" +
+        "3. Study");
+    }
 }
