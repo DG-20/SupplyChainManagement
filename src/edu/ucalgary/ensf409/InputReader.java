e3c2202867b9fec49de8b8c2fac85c3c8dd6ddb6
LiamParmar <Liam.Parmar13@gmail.com>
2021-03-30 23:07:17 -0600

    displayChairs Method Completed

--- src/edu/ucalgary/ensf409/InputReader.java
+++ src/edu/ucalgary/ensf409/InputReader.java
@@ -3,7 +3,19 @@ package edu.ucalgary.ensf409;
 public class InputReader {
     public static void main(String[] args) {
         InputReader obj = new InputReader();
+
+        displayChairs();
+        
     }
 
-    
+    public static void displayChairs()
+    {
+        System.out.println("You selected chairs as your furniture.\n" +
+        "Please select from the following types of chairs: \n" + 
+        "1. Ergonomic \n" +
+        "2. Executive \n" +
+        "3. Kneeling \n" +
+        "4. Mesh \n" +
+        "5. Task \n");
+    }
 }
