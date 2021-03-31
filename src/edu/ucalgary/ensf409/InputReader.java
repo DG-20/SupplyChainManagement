794cce14d854f36140423cd6ae764a67c74b5f94
Maheen <maheen.shossain@gmail.com>
2021-03-30 23:11:03 -0600

    Done displayFiling

--- src/edu/ucalgary/ensf409/InputReader.java
+++ src/edu/ucalgary/ensf409/InputReader.java
@@ -3,7 +3,17 @@ package edu.ucalgary.ensf409;
 public class InputReader {
     public static void main(String[] args) {
         InputReader obj = new InputReader();
+
+        
     }
 
-    
+    public void displayFiling() {
+
+        System.out.println("You selected filing as your furniture.\n" +
+        "Please select from the following types of filing: \n" + 
+        "1. Small \n" +
+        "2. Medium \n" +
+        "3. Large ");
+
+    }
 }
