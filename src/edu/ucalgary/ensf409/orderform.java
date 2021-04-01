c9ab22df3b43698a24a762872ba3ac76f2748406
curtissilva <curtissilva77@gmail.com>
2021-04-01 01:09:44 -0600

    Orderform text file updates

--- src/edu/ucalgary/ensf409/orderform.java
+++ src/edu/ucalgary/ensf409/orderform.java
@@ -5,15 +5,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class orderform extends DataBaseManipulator {
+public class OrderForm extends DataBaseManipulator {
     public static void main(String[] args) throws IOException {
         String url = "jdbc:mysql://localhost/inventory";
         String username = "scm";
         String password = "ensf409";
-        orderform object = new orderform(url, username, password);
+        OrderForm object = new OrderForm(url, username, password);
     }
 
-    public orderform(String url, String username, String password) throws IOException {
+    public OrderForm(String url, String username, String password) throws IOException {
         super(url, username, password);
         finalOrderTextFile(super.furnitureChosen, super.typeChosen, super.quantity);
     }
@@ -21,13 +21,18 @@ public class orderform extends DataBaseManipulator {
     public void finalOrderTextFile(String furniture, String type, int quantity) throws IOException {
         System.out.println("Printing Furniture Order Form...\n");
         String outputForm = "";
-        outputForm = "Furniture Order Form\n" + "\n" + "Faculty Name:     " + "\n" + "Contact:      \n" + "Date:     \n"
-                + "\n";
+        outputForm = "┎                                                          ┓\n";
+        outputForm = outputForm.concat("\nFurniture Order Form\n");
+        //outputForm = outputForm.concat("✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂✂\n");
+        outputForm = outputForm.concat("┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅\n");
+        outputForm = outputForm.concat("Faculty Name:     " + "\n" + "Contact:      \n" + "Date:     \n"
+        + "\n");
         outputForm = outputForm
                 .concat("Original Request: " + type + " " + furniture + ", " + Integer.toString(quantity) + "\n");
         outputForm = outputForm.concat("\n" + "Items Ordered" + "\n");
         outputForm = outputForm.concat("ID: ");
         outputForm = outputForm.concat("\n\n" + "Total Price: ");
+        outputForm = outputForm.concat("\n\n┗                                                          ┛\n");
         FileWriter fileOutput = new FileWriter("orderform.txt");
         fileOutput.write(outputForm);
         fileOutput.close();
