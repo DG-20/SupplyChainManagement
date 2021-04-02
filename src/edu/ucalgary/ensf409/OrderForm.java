42ead57259e9355fc3d749728f5a9c14874e3ba6
LiamParmar <Liam.Parmar13@gmail.com>
2021-04-02 00:36:03 -0600

    Commenting of OrderForm

--- src/edu/ucalgary/ensf409/OrderForm.java
+++ src/edu/ucalgary/ensf409/OrderForm.java
@@ -7,18 +7,33 @@ import java.util.Scanner;
 
 public class OrderForm extends DataBaseManipulator {
     public static void main(String[] args) throws IOException {
-        String url = "jdbc:mysql://localhost/inventory";
-        String username = "scm";
-        String password = "ensf409";
-        OrderForm object = new OrderForm(url, username, password);
+        String url = "jdbc:mysql://localhost/inventory"; 
+        String username = "scm"; 
+        String password = "ensf409"; 
+        OrderForm object = new OrderForm(url, username, password); 
     }
 
-    public OrderForm(String url, String username, String password) throws IOException {
-        super(url, username, password);
+    /* The constructor OrderForm takes in 3 arguments: A string containing the path
+    *  to the file, a string for the username of the host, and a string for the password
+    *  to the host connection. It then uses the stored values of the furniture, the type
+    *  of the furniture, and the quantity desired to the creation method of the finalOrder.
+    */
+    public OrderForm(String url, String username, String password) throws IOException 
+    {
+        super(url, username, password); 
         finalOrderTextFile(super.furnitureChosen, super.typeChosen, super.quantity);
     }
 
-    public void finalOrderTextFile(String furniture, String type, int quantity) throws IOException {
+    /* The following method finalOrderTextFile generates a order form and concatonates 
+    *  many different strings that format and that are added onto the form from the 
+    *  database. The method takes in 3 arguments: A string for the desired furniture, 
+    *  a string for the type of furniture selected, and the quantity desired for that
+    *  type of furniture. The formatting alongs with the various inputted strings are 
+    *  then written to an output file called orderform. The orderform is then closed.
+    */ 
+
+    public void finalOrderTextFile(String furniture, String type, int quantity) throws IOException 
+    {
         System.out.println("Printing Furniture Order Form...\n");
         String outputForm = "";
         outputForm = "┎                                                          ┓\n";
